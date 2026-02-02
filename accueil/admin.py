97ee005f161c5df8bd8febd2214f9a0b8f6bd5a8
edmond <edmond.y@h2altitude.com>
2026-02-02 11:54:14 +0100

    Feat: Ajout du style CSS pour la liste des tâches

--- accueil/admin.py
+++ accueil/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tache
 
-# Register your models here.
+
+@admin.register(Tache)
+class TacheAdmin(admin.ModelAdmin):
+	list_display = ('titre', 'terminee')
+	list_filter = ('terminee',)
+	search_fields = ('titre',)
--- accueil/models.py
+++ accueil/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Tache(models.Model):
+	"""Modèle représentant une tâche simple.
+
+	Champs:
+	- titre: texte (max 200 caractères)
+	- terminee: booléen indiquant si la tâche est terminée (False par défaut)
+	"""
+	titre = models.CharField(max_length=200)
+	terminee = models.BooleanField(default=False)
+
+	def __str__(self):
+		# Affichage lisible dans l'admin et les reprsentation texte
+		etat = 'terminée' if self.terminee else 'en cours'
+		return f"{self.titre} ({etat})"
