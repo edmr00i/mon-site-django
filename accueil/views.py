7973f67871a97c0a40001e248a3f9e5c2fb7c151
edmond <edmond.y@h2altitude.com>
2026-02-02 11:31:30 +0100

    Modif: Mise à jour du texte sur la page d'accueil

--- accueil/views.py
+++ accueil/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render
-from datetime import date
+from datetime import date, datetime
 from .forms import NomForm
 
 def index(request):
-    message = "Bonjour, le monde de Django !"
+    """Vue index qui affiche un message dépendant de l'heure et gère un formulaire de nom."""
+    # Déterminer le salut selon l'heure locale (module datetime demandé)
+    maintenant = datetime.now()
+    if maintenant.hour < 12:
+        greeting = "Bon matin"
+    else:
+        greeting = "Bon après-midi"
+
+    # Valeur par défaut du message
+    message = f"{greeting} !"
     nom = None
 
     if request.method == "POST":
         form = NomForm(request.POST)
         if form.is_valid():
             nom = form.cleaned_data['nom']
-            message = f"Bonjour, {nom} !"
+            # Message personnalisé avec le nom
+            message = f"{greeting}, {nom} !"
     else:
         form = NomForm()
+
+    # Liste de tâches à afficher sur la page d'accueil (exemple statique)
+    tasks = [
+        "Acheter du lait",
+        "Terminer la formation Django",
+        "Envoyer l'email de suivi"
+    ]
 
     contexte = {
         'message': message,
         'date_du_jour': date.today(),
-        'form': form
+        'form': form,
+        'tasks': tasks,
     }
     return render(request, 'accueil/index.html', contexte)
